12bdfd682dcc9d0acbe539601c27172c53c4c8fe
Florian Wendelborn <dodekeract@users.noreply.github.com>
2017-01-01 19:59:38 +0100

    .

--- source/person.js
+++ source/person.js
@@ -13,6 +13,10 @@ import state from './state'
 export const createPerson = ({name, gender}) => ({
 	name,
 	gender,
+	children: [],
+	father: null,
+	mother: null,
+	partner: null,
 	id: uuid()
 })
 
@@ -21,7 +25,24 @@ export const createPerson = ({name, gender}) => ({
 // region add
 
 export const addChild = (_parent, _child) => {
-	state.get()
+	const parent = state.get(_parent)
+	const child = state.get(_child)
+	if (parent.gender === 'm') {
+		if (child.father) return false
+		child.father = _parent
+		parent.children.push(_child)
+		parent.children = [...new Set(parent.children)]
+		state.save(father)
+		state.save(child)
+	} else {
+		if (child.mother) return false
+		child.mother = _parent
+		parent.children.push(_child)
+		parent.children = [...new Set(parent.children)]
+		state.save(parent)
+		state.save(child)
+	}
+	return true
 }
 
 // endregion
